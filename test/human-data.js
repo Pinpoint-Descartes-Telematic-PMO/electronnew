83d5ddf8234e73043938c937351e6c31f803c9ff
HashimotoYT <hashimoto.stream@gmail.com>
2020-03-24 21:45:05 +0200

    chore: fixup

--- test/human-data.js
+++ test/human-data.js
@@ -44,7 +44,7 @@ describe('human-submitted app data', () => {
       })
 
       describe(`${yamlFile}`, () => {
-        const app = yaml.safeLoad(yamlPath)
+        const app = yaml.safeLoad(fs.readFileSync(yamlPath))
 
         it('has a name', () => {
           expect(app.name.length).to.be.above(0)
